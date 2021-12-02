43ead763b512b742beb0147b690b94c3893338e1
jbuget <jbuget@gmail.com>
2021-12-02 19:38:41 +0100

    day/2

--- day/2/index.js
+++ day/2/index.js
@@ -1,9 +1,40 @@
 const fs = require('fs');
 
 function partOne(data) {
-
+  let horizontalPosition = 0;
+  let depth = 0;
+  data.split('\n').forEach((line) => {
+    if (line[0] === 'f') {
+      horizontalPosition += parseInt(line.split('forward ')[1]);
+    }
+    if (line[0] === 'u') {
+      depth -= parseInt(line.split('up ')[1]);
+    }
+    if (line[0] === 'd') {
+      depth += parseInt(line.split('down ')[1]);
+    }
+  });
+  return horizontalPosition * depth;
 }
+
 function partTwo(data) {
+  let horizontalPosition = 0;
+  let depth = 0;
+  let aim = 0;
+  data.split('\n').forEach((line) => {
+    if (line[0] === 'f') {
+      const value = parseInt(line.split('forward ')[1]);
+      horizontalPosition += value;
+      depth += aim * value;
+    }
+    if (line[0] === 'u') {
+      aim -= parseInt(line.split('up ')[1]);
+    }
+    if (line[0] === 'd') {
+      aim += parseInt(line.split('down ')[1]);
+    }
+  });
+  return horizontalPosition * depth;
 
 }
 
@@ -13,8 +44,8 @@ function main() {
     const data = fs.readFileSync(`${__dirname}/input`, 'utf8');
 
     // Process
-    const result = partOne(data);
-    //const result = partTwo(data);
+    //const result = partOne(data);
+    const result = partTwo(data);
 
     // Output
     console.log(result);
