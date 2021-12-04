cad58dec6e0b7556edcc2cce0b3121cfef8ab810
jbuget <jbuget@gmail.com>
2021-12-04 13:42:18 +0100

    day/4 part one

---
+++ day/4/index.js
@@ -0,0 +1,124 @@
+const BOARD_SIZE = 5;
+
+class BoardNumber {
+  value;
+  isMarked;
+
+  constructor(value) {
+    this.value = value;
+    this.isMarked = false;
+  }
+
+  mark() {
+    this.isMarked = true;
+  }
+}
+
+class Board {
+  numbers;
+  lastMarkedNumberValue;
+
+  constructor() {
+    this.numbers = new Array(BOARD_SIZE);
+    for (let i = 0 ; i < BOARD_SIZE ; i++) {
+      this.numbers[i] = new Array(BOARD_SIZE);
+    }
+    this.lastMarkedNumberValue = null;
+  }
+
+  addNumber(value, x, y) {
+    this.numbers[x][y] = new BoardNumber(value);
+  }
+
+  get allNumbers() {
+    return this.numbers.reduce((result, rows) => result.concat(rows), []);
+  }
+
+  markNumber(value) {
+    const correspondingNumber = this.allNumbers.find((number) => number.value === value);
+    if (correspondingNumber) {
+      correspondingNumber.mark();
+      this.lastMarkedNumberValue = value;
+    }
+  }
+
+  isWinning() {
+    // By rows
+    for (let i = 0 ; i < BOARD_SIZE ; i++) {
+      const firstUnmarkedNumber = this.numbers[i].find((number) => !number.isMarked);
+      if (!firstUnmarkedNumber) {
+        return true;
+      }
+    }
+    // By columns
+    for (let i = 0 ; i < BOARD_SIZE ; i++) {
+      const columnNumbers = [];
+      for (let j = 0 ; j < BOARD_SIZE ; j++) {
+        columnNumbers.push(this.numbers[j][i]);
+      }
+      const firstUnmarkedNumber = columnNumbers.find((number) => !number.isMarked);
+      if (!firstUnmarkedNumber) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  getScore() {
+    const sumOfUnmarkedNumbers = this.allNumbers
+      .filter((number) => !number.isMarked)
+      .reduce((result, number) => result + parseInt(number.value), 0);
+    return sumOfUnmarkedNumbers * parseInt(this.lastMarkedNumberValue);
+  }
+}
+
+class Game {
+  drawns;
+  boards;
+
+  constructor(drawns, boards) {
+    this.drawns = drawns;
+    this.boards = boards;
+  }
+
+  run() {
+    let winningBoard = null;
+    while (!winningBoard) {
+      const currentDrawnNumber = this.drawns.shift();
+      this.boards.forEach((board) => board.markNumber(currentDrawnNumber));
+      winningBoard = this.boards.find((board) => board.isWinning());
+    }
+    return winningBoard;
+  }
+}
+
+function buildGameFromInput(data) {
+  const lines = data.split('\n');
+  const drawns = lines[0].split(',');
+  const boards = [];
+
+  for (let i = 2 ; i < lines.length ; i++) {
+    const board = new Board();
+    for (let j = 0 ; j < BOARD_SIZE ; j++) {
+      const line = lines[i+j].trim().replace(/\s{2,}/g, ' ');
+      line.split(' ').forEach((value, k) => {
+        board.addNumber(value.trim(), j, k);
+      });
+    }
+    i = i + BOARD_SIZE;
+    boards.push(board);
+  }
+  return new Game(drawns, boards);
+}
+
+function partOne(data) {
+  const game = buildGameFromInput(data);
+  const winningBoard = game.run();
+  return winningBoard.getScore();
+}
+
+function partTwo(data) {
+  // TODO
+}
+
+module.exports = [partOne, partTwo];
