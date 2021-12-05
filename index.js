670a576cf8afd9fb3c4bffc82e7cacc4cdb4693a
jbuget <jbuget@gmail.com>
2021-12-05 17:29:48 +0100

    Improve index.js: log execution time

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ const fs = require('fs');
 function run(day, part) {
   try {
     // Input
+    console.time('time');
+
     const parts = require(`${__dirname}/day/${day}`);
-    const data = fs.readFileSync(`${__dirname}/day/${day}/input.txt`, 'utf8');
+    const data = fs.readFileSync(`${__dirname}/day/${day}/input.txt`, 'utf8').trim();
 
     // Process
     let result = (part === 'one') ? parts[0](data) : parts[1](data);
 
     // Output
-    console.log(result);
+    console.log('result:', result);
+    console.timeEnd('time');
   } catch (err) {
     console.error(err);
     process.exit(1);
   }
 }
-
-console.log('day:', process.argv[2]);
-console.log('part:', process.argv[3]);
 
 run(process.argv[2], process.argv[3]);
