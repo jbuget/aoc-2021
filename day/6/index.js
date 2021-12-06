257d6875aa1d995d997f1d7ec805d7616f5e4537
jbuget <jbuget@gmail.com>
2021-12-06 14:35:31 +0100

    day/6 remove useless code & methods

--- day/6/index.js
+++ day/6/index.js
@@ -1,145 +1,4 @@
-class Lanternfish {
-  timer;
-  children;
-
-  constructor(timer) {
-    this.timer = timer;
-    this.children = [];
-  }
-
-  addChild(baby) {
-    this.children.push(baby);
-  }
-
-  get length() {
-    return this.children.reduce((result, child) => {
-      return result + child.length;
-    }, 1);
-  }
-
-  processADay() {
-    this.timer--;
-
-    this.children.forEach((child) => child.processADay());
-
-    if (this.timer < 0) {
-      this.timer = 6;
-      this.addChild(new Lanternfish(8));
-    }
-  }
-}
-
-function live1(values, nbDays) {
-  const lanternfish = new Lanternfish(values.shift());
-  for (let i = 0; i < values.length; i++) {
-    lanternfish.addChild(new Lanternfish(values[i]));
-  }
-  for (let d = 0 ; d < nbDays ; d++) {
-    lanternfish.processADay();
-  }
-  return lanternfish.length;
-}
-
-function live2(values, nbDays) {
-  const colony = [];
-  for (let i = 0; i < values.length; i++) {
-    colony.push(values[i]);
-  }
-  for (let d = 0 ; d < nbDays ; d++) {
-    let colonyLength = colony.length;
-    for (let l = 0 ; l < colonyLength ; l++) {
-      const fishTimer = --colony[l];
-      if (fishTimer < 0) {
-        colony[l] = 6;
-        colony.push(8);
-      }
-    }
-  }
-  return colony.length;
-}
-
-function live3(values, nbDays) {
-  const generations = [];
-
-  const firstGeneration = [];
-  for (let i = 0; i < values.length; i++) {
-    firstGeneration.push(values[i]);
-  }
-  generations.push(firstGeneration);
-
-  for (let d = 0 ; d < nbDays ; d++) {
-    const newGeneration = [];
-
-    for (let g = 0 ; g < generations.length ; g++) {
-      const generation = generations[g];
-      for (let f = 0 ; f < generation.length ; f++) {
-        generation[f]--;
-        if (generation[f] < 0) {
-          generation[f] = 6;
-          newGeneration.push(8);
-        }
-      }
-    }
-    generations.push(newGeneration);
-  }
-
-  return generations.reduce((result, generation) => result + generation.length, 0);
-}
-
-function live4(values, nbDays) {
-  let colony = Uint8Array.from(values);
-  for (let d = 0 ; d < nbDays ; d++) {
-    let nbFishesToAdd = 0;
-    for (let f = 0 ; f < colony.length ; f++) {
-      let fishTimer = colony[f];
-      fishTimer--;
-      if (fishTimer < 0) {
-        colony[f] = 6;
-        nbFishesToAdd++;
-      } else {
-        colony[f] = fishTimer;
-      }
-    }
-
-    const newColony = new Uint8Array(nbFishesToAdd);
-    for (let j = 0 ; j < nbFishesToAdd ; j++) {
-      newColony[j] = 8;
-    }
-
-    const nextColony = new Uint8Array(colony.length + nbFishesToAdd);
-    nextColony.set(colony, 0);
-    nextColony.set(newColony, colony.length);
-    colony = nextColony;
-  }
-  return colony.length;
-}
-
-function live5(values, nbDays) {
-  const generations = [];
-
-  const firstGeneration = Uint8Array.from(values);
-  generations.push(firstGeneration);
-
-  for (let d = 0 ; d < nbDays ; d++) {
-    let nbFishesToAdd = 0;
-    for (let g = 0 ; g < generations.length ; g++) {
-      const generation = generations[g];
-      for (let f = 0 ; f < generation.length ; f++) {
-        if (generation[f]-- === 0) {
-          generation[f] = 6;
-          nbFishesToAdd++;
-        }
-      }
-    }
-    const newGeneration = new Uint8Array(nbFishesToAdd);
-    newGeneration.fill(8);
-    generations.push(newGeneration);
-  }
-
-  return generations.reduce((result, generation) => result + generation.length, 0);
-}
-
-function live6(values, nbDays) {
+function evaluatePopulation(values, nbDays) {
   const fishesByAge = Array(9);
   fishesByAge.fill(0);
 
@@ -161,12 +20,12 @@ function live6(values, nbDays) {
 
 function partOne(data) {
   let values = data.split(',').map((value) => parseInt(value));
-  return live6(values, 80);
+  return evaluatePopulation(values, 80);
 }
 
 function partTwo(data) {
   let values = data.split(',').map((value) => parseInt(value));
-  return live6(values, 256);
+  return evaluatePopulation(values, 256);
 }
 
-module.exports = { partOne, partTwo, Lanternfish };
+module.exports = { partOne, partTwo };
