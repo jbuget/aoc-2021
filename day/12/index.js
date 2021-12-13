3248924d0828a3296ed0cfd0f3e33822811a9767
jbuget <jbuget@gmail.com>
2021-12-13 01:22:20 +0100

    day/12 part one

--- day/12/index.js
+++ day/12/index.js
@@ -1,5 +1,57 @@
+class Cave {
+  name;
+  neighbors;
+
+  constructor(name) {
+    this.name = name;
+    this.neighbors = [];
+  }
+
+  addNeighbor(cave) {
+    this.neighbors.push(cave);
+  }
+
+  get isSmallCave() {
+    return this.name[0] >= 'a' && this.name[0] <= 'z';
+  }
+}
+
+function buildCaves(data) {
+  return data.split('\n').reduce((caves, line) => {
+    const [from, to] = line.split('-');
+    if (!caves[from]) {
+      caves[from] = new Cave(from);
+    }
+    if (!caves[to]) {
+      caves[to] = new Cave(to);
+    }
+    caves[from].addNeighbor(caves[to]);
+    caves[to].addNeighbor(caves[from]);
+    return caves;
+  }, {});
+}
+
+
+function dfs(cave, visited, paths) {
+  visited.push(cave.name);
+  if (cave.name === 'end') {
+    paths.push(visited.join(','));
+    return;
+  }
+  for (const neighbor of cave.neighbors) {
+    if (neighbor.isSmallCave && visited.includes(neighbor.name)) {
+      continue;
+    }
+    dfs(neighbor, [...visited], paths);
+  }
+}
+
+
 function partOne(data) {
-  return 'TODO';
+  const caves = buildCaves(data);
+  const paths = [];
+  dfs(caves['start'], [], paths);
+  return paths.length;
 }
 
 function partTwo(data) {
--- day/12/index.test.js
+++ day/12/index.test.js
@@ -1,16 +1,41 @@
 const fs = require('fs');
 const { partOne, partTwo } = require('./index');
 
-const data = fs.readFileSync(`${__dirname}/input.test.txt`, 'utf8').trim();
-
 describe('part one', () => {
 
-  it('should ', () => {
+  it('small', () => {
+    // given
+    const data = fs.readFileSync(`${__dirname}/input_1.test.txt`, 'utf8').trim();
+
     // when
     const actual = partOne(data);
 
     // then
-    const expected = 'TODO';
+    const expected = 10;
+    expect(actual).toStrictEqual(expected);
+  });
+
+  it('medium', () => {
+    // given
+    const data = fs.readFileSync(`${__dirname}/input_2.test.txt`, 'utf8').trim();
+
+    // when
+    const actual = partOne(data);
+
+    // then
+    const expected = 19;
+    expect(actual).toStrictEqual(expected);
+  });
+
+  it('large', () => {
+    // given
+    const data = fs.readFileSync(`${__dirname}/input_3.test.txt`, 'utf8').trim();
+
+    // when
+    const actual = partOne(data);
+
+    // then
+    const expected = 226;
     expect(actual).toStrictEqual(expected);
   });
 });
