de72cec379e2c412cde3c9e08b845f0c638b0a3d
jbuget <jbuget@gmail.com>
2021-12-18 15:40:57 +0100

    day/16 part one

--- day/16/index.js
+++ day/16/index.js
@@ -1,9 +1,91 @@
+/* https://stackoverflow.com/a/68315766 */
+function hex2bin(hexadecimalString) {
+  return hexadecimalString.split('').map(i => parseInt(i, 16).toString(2).padStart(4, '0')).join('');
+}
+
+class Packet {
+
+  version;
+  typeID;
+  literalValue;
+  subpackets;
+
+  constructor(version, typeID, options = {}) {
+    this.version = version;
+    this.typeID = typeID;
+    this.literalValue = options.literalValue;
+    this.subpackets = options.subpackets;
+  }
+}
+
+function readLiteralValuePacket(binaryString, start, end, version) {
+  let i = start;
+  let valueString = '';
+  let lastGroupParsed = false;
+  let group = binaryString.substring(i, i + 5);
+  while (i < binaryString.length && i < end && group.length === 5 && !lastGroupParsed) {
+    if (group[0] === '0') {
+      lastGroupParsed = true;
+    }
+    valueString += group.substring(1);
+    i += 5;
+    group = binaryString.substring(i, i + 5);
+  }
+  const literalValue = parseInt(valueString, 2);
+  const packet = new Packet(version, 4, { literalValue });
+  return { packet, offset: i };
+}
+
+function getPacketsFromBinaryTransmission(binaryString, start, end, nbPackets = Infinity, packetTypeID) {
+  let packets = [];
+  let offset = start;
+  while (offset < end && packets.length < nbPackets) {
+    if (!packetTypeID && (end - offset) < 11) { // end padding
+      offset = end;
+      continue;
+    }
+    const version = parseInt(binaryString.substring(offset, offset + 3), 2);
+    offset += 3;
+
+    const typeID = parseInt(binaryString.substring(offset, offset + 3), 2);
+    offset += 3;
+
+    if (typeID === 4) {
+      const { packet, offset:i} = readLiteralValuePacket(binaryString, offset, binaryString.length - 1, version);
+      packets.push(packet);
+      offset = i;
+    } else {
+      const lengthTypeID = parseInt(binaryString.substring(offset, offset + 1), 2);
+      offset += 1;
+
+      let endOffset, nbSubpackets;
+
+      if (lengthTypeID === 0 ) {
+        endOffset = offset + 15 + parseInt(binaryString.substring(offset, offset + 15), 2);
+        offset += 15;
+        nbSubpackets = Infinity;
+      } else {
+        endOffset = binaryString.length - 1;
+        nbSubpackets = parseInt(binaryString.substring(offset, offset + 11), 2);
+        offset += 11;
+      }
+      const { packets: subpackets, offset:i } = getPacketsFromBinaryTransmission(binaryString, offset, endOffset, nbSubpackets, typeID);
+      const packet = new Packet(version, typeID, { subpackets });
+      packets.push(packet, ...subpackets);
+      offset = i;
+    }
+  }
+  return { packets, offset };
+}
+
 function partOne(data) {
-  return 'TODO';
+  const binaryTransmission = hex2bin(data);
+  const { packets } = getPacketsFromBinaryTransmission(binaryTransmission, 0, binaryTransmission.length - 1);
+  return packets.reduce((sum, packet) => (sum + packet.version), 0);
 }
 
 function partTwo(data) {
   return 'TODO';
 }
 
-module.exports = { partOne, partTwo };
+module.exports = { partOne, partTwo, hex2bin, getPacketsFromBinaryTransmission, Packet };
--- day/16/index.test.js
+++ day/16/index.test.js
@@ -1,21 +1,173 @@
 const fs = require('fs');
-const { partOne, partTwo } = require('./index');
+const { partOne, partTwo, hex2bin, getPacketsFromBinaryTransmission, Packet } = require('./index');
 
-const data = fs.readFileSync(`${__dirname}/input.test.txt`, 'utf8').trim();
+xdescribe('#getPacketsFromBinaryTransmission', () => {
 
-describe('part one', () => {
+  [{
+    input: 'D2FE28', expected: '110100101111111000101000',
+    input: '38006F45291200', expected: '00111000000000000110111101000101001010010001001000000000',
+    input: 'EE00D40C823060', expected: '11101110000000001101010000001100100000100011000001100000',
+  }].forEach((usecase) => {
+    it(`Packet#versionSum for "${usecase.input}" should be '${usecase.expected}')`, () => {
+      // const
+      const binaryTransmission = hex2bin(usecase.input);
 
-  it('should ', () => {
-    // when
-    const actual = partOne(data);
+      // when
+      const actual = getPacketsFromBinaryTransmission(binaryTransmission);
 
-    // then
-    const expected = 'TODO';
-    expect(actual).toStrictEqual(expected);
+      // then
+      expect(actual).toStrictEqual(usecase.expected);
+    });
   });
 });
 
-describe('part two', () => {
+/*
+describe('class Packet', () => {
+
+  describe('#version', () => {
+
+    [{
+      input: '110100101111111000101000', expected: 6,
+      input: '00111000000000000110111101000101001010010001001000000000', expected: 1,
+      input: '11101110000000001101010000001100100000100011000001100000', expected: 7,
+    }].forEach((usecase) => {
+      it(`Packet#version for "${usecase.input}" should be "${usecase.expected}"`, () => {
+        // given
+        const packet = new Packet(usecase.input)
+
+        // when
+        const actual = packet.version;
+
+        // then
+        expect(actual).toBe(usecase.expected);
+      });
+    });
+  });
+
+  describe('#typeID', () => {
+
+    [{
+      input: '110100101111111000101000', expected: 4,
+      input: '00111000000000000110111101000101001010010001001000000000', expected: 6,
+      input: '11101110000000001101010000001100100000100011000001100000', expected: 3,
+    }].forEach((usecase) => {
+      it(`Packet#typeID for "${usecase.input}" should be "${usecase.expected}"`, () => {
+        // given
+        const packet = new Packet(usecase.input)
+
+        // when
+        const actual = packet.typeID;
+
+        // then
+        expect(actual).toBe(usecase.expected);
+      });
+    });
+  });
+
+  describe('#isLiteralValue', () => {
+
+    it('should return "true" only if typeID does equal 4', () => {
+      // true
+      expect(new Packet('110100101111111000101000').isLiteralValue).toBeTruthy();
+
+      // false
+      expect(new Packet('00111000000000000110111101000101001010010001001000000000').isLiteralValue).toBeFalsy();
+      expect(new Packet('11101110000000001101010000001100100000100011000001100000').isLiteralValue).toBeFalsy();
+    });
+  });
+
+  describe('#literalValue', () => {
+
+    it('should be ok', () => {
+      // given
+      const packet = new Packet('D2FE28', true);
+
+      // when
+      const literalValue = packet.literalValue;
+
+      // then
+      expect(literalValue).toBe(2021);
+    });
+  });
+
+
+  describe('#isOperator', () => {
+
+    it('should return "true" only if typeID does not equal 4', () => {
+      // true
+      expect(new Packet('110100101111111000101000').isOperator).toBeFalsy();
+
+      // false
+      expect(new Packet('00111000000000000110111101000101001010010001001000000000').isOperator).toBeTruthy();
+      expect(new Packet('11101110000000001101010000001100100000100011000001100000').isOperator).toBeTruthy();
+    });
+  });
+
+  describe('#lengthTypeID', () => {
+
+    it('should be ok', () => {
+      expect(new Packet('00111000000000000110111101000101001010010001001000000000').lengthTypeId).toBe(0);
+      expect(new Packet('11101110000000001101010000001100100000100011000001100000').lengthTypeId).toBe(1);
+    });
+  });
+
+  describe('#subpacketsTotalLength', () => {
+
+    it('should be ok', () => {
+      expect(new Packet('00111000000000000110111101000101001010010001001000000000').subpacketsTotalLength).toBe(27);
+    });
+  });
+
+  describe('#subpacketsNumber', () => {
+
+    it('should be ok', () => {
+      expect(new Packet('11101110000000001101010000001100100000100011000001100000').subpacketsNumber).toBe(3);
+    });
+  });
+
+  xdescribe('#versionSum', () => {
+
+    [{
+      input: '8A004A801A8002F478', expected: 16,
+      input: '620080001611562C8802118E34', expected: 12,
+      input: 'C0015000016115A2E0802F182340', expected: 23,
+      input: 'A0016C880162017C3686B18A3D4780', expected: 31,
+    }].forEach((usecase) => {
+      it(`Packet#versionSum for "${usecase.input}" should be '${usecase.expected}')`, () => {
+        // given
+        const packet = new Packet(usecase.input);
+
+        // when
+        const actual = packet.versionSum;
+
+        // then
+        expect(actual).toStrictEqual(usecase.expected);
+      });
+    });
+  });
+});
+*/
+
+describe('part one', () => {
+
+  [
+    { input: 'D2FE28', expected: 6 },
+    { input: '8A004A801A8002F478', expected: 16 },
+    { input: '620080001611562C8802118E34', expected: 12 },
+    { input: 'C0015000016115A2E0802F182340', expected: 23 },
+    { input: 'A0016C880162017C3686B18A3D4780', expected: 31 },
+  ].forEach((usecase) => {
+    it(`${usecase.input} should be ${usecase.expected}`, () => {
+      // when
+      const actual = partOne(usecase.input);
+
+      // then
+      expect(actual).toStrictEqual(usecase.expected);
+    });
+  })
+});
+
+xdescribe('part two', () => {
 
   it('should ', () => {
     // when
