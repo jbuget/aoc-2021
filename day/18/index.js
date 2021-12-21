3214532e02576a02d98f0576e6196bf135b7c505
jbuget <jbuget@gmail.com>
2021-12-21 01:05:29 +0100

    day/18 part two

--- day/18/index.js
+++ day/18/index.js
@@ -6,7 +6,22 @@ function partOne(data) {
 }
 
 function partTwo(data) {
-  return 'TODO';
+  const snailfishNumbers = data.split('\n');
+
+  let largestMagnitude = 0;
+  for (let i = 0 ; i < snailfishNumbers.length ; i++) {
+    for (let j = 0 ; j < snailfishNumbers.length ; j++) {
+      if (i !== j) {
+        const left = snailfishNumbers[i];
+        const right = snailfishNumbers[j];
+        const mag = magnitude(sum([left, right]));
+        if (mag > largestMagnitude) {
+          largestMagnitude = mag;
+        }
+      }
+    }
+  }
+  return largestMagnitude;
 }
 
 module.exports = { partOne, partTwo };
