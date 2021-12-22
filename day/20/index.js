4bd23810dc4563ae2dde7f3a46ab5981c3443472
jbuget <jbuget@gmail.com>
2021-12-22 23:43:58 +0100

    day/20 prepare some part one tests

--- day/20/index.js
+++ day/20/index.js
@@ -1,4 +1,28 @@
+function selectInputPixels(inputImage, inputPixel) {
+
+}
+
+function enhanceImage(imageEnhancementAlgorithm, inputImage) {
+
+}
+
+function countLitPixels(image) {
+
+}
+
 function partOne(data) {
+  const lines = data.split('\n');
+  const imageEnhancementAlgorithm = lines[0];
+
+  const inputImage = [];
+  for (let i = 2 ; i < lines.length ; i++) {
+    inputImage.push(lines[i]);
+  }
+
+  console.log(imageEnhancementAlgorithm);
+
+  inputImage.forEach(console.log);
+
   return 'TODO';
 }
 
@@ -6,4 +30,4 @@ function partTwo(data) {
   return 'TODO';
 }
 
-module.exports = { partOne, partTwo };
+module.exports = { partOne, partTwo, selectInputPixels, convertPixelsToBinaryNumber, getOutputPixelFromBinaryNumber, countLitPixels };
--- day/20/index.test.js
+++ day/20/index.test.js
@@ -1,7 +1,97 @@
 const fs = require('fs');
-const { partOne, partTwo } = require('./index');
+const {
+  partOne,
+  partTwo,
+  selectInputPixels,
+  convertPixelsToBinaryNumber,
+  getOutputPixelFromBinaryNumber,
+  countLitPixels
+} = require('./index');
 
 const data = fs.readFileSync(`${__dirname}/input.test.txt`, 'utf8').trim();
+
+describe('#selectInputPixels', () => {
+
+  it('should ', () => {
+    // given
+    const inputImage = [
+      '#..#.',
+      '#....',
+      '##..#',
+      '..#..',
+      '..###',
+    ];
+
+    // when
+    const actual = selectInputPixels(inputImage, [5, 10]);
+
+    // then
+    expect(actual).toStrictEqual('...#...#.');
+  });
+});
+
+describe('#convertPixelsToBinaryNumber', () => {
+
+  it('middle pixel [5,10]', () => {
+    // when
+    const actual = convertPixelsToBinaryNumber('...#...#.');
+
+    // then
+    expect(actual).toStrictEqual('000100010');
+  });
+
+  it('top-left pixel [0,0]', () => {
+    // when
+    const actual = convertPixelsToBinaryNumber('...#...#.');
+
+    // then
+    expect(actual).toStrictEqual('000100010');
+  });
+});
+
+describe('#getOutputPixelFromBinaryNumber', () => {
+
+  it('should ', () => {
+    // given
+    const imageEnhancementAlgorithm = '..#.#..#####.#.#.#.###.##.....###.##.#..###.####..';
+
+    // when
+    const actual = getOutputPixelFromBinaryNumber(imageEnhancementAlgorithm, '000100010');
+
+    // then
+    expect(actual).toStrictEqual('#');
+  });
+});
+
+describe('#countLitPixels', () => {
+
+  it('should ', () => {
+    // given
+    const image =  [
+      '...............',
+      '...............',
+      '...............',
+      '..........#....',
+      '....#..#.#.....',
+      '...#.#...###...',
+      '...#...##.#....',
+      '...#.....#.#...',
+      '....#.#####....',
+      '.....#.#####...',
+      '......##.##....',
+      '.......###.....',
+      '...............',
+      '...............',
+      '...............',
+    ];
+
+    // when
+    const actual =countLitPixels(image);
+
+    // then
+    expect(actual).toStrictEqual(35);
+  });
+});
 
 describe('part one', () => {
 
@@ -10,7 +100,7 @@ describe('part one', () => {
     const actual = partOne(data);
 
     // then
-    expect(actual).toStrictEqual('TODO');
+    expect(actual).toStrictEqual(35);
   });
 });
 
